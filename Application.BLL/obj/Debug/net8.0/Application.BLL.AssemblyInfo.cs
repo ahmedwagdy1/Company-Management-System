b3417ac8231f3cbe49865a5faa3072627f6f7060
ahmedwagdy1 <wagdyy2267@gmail.com>
2026-04-30 07:24:32 +0300

    ignore vs folder

--- Application.BLL/obj/Debug/net8.0/Application.BLL.AssemblyInfo.cs
+++ Application.BLL/obj/Debug/net8.0/Application.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1055eaf7cc848e80c2cf4450c6c3e4a8b9dbf240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ffc2a4116fc8a19e432b441d90f17f6e00f9f95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Application.DAL/obj/Debug/net8.0/Application.DAL.AssemblyInfo.cs
+++ Application.DAL/obj/Debug/net8.0/Application.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1055eaf7cc848e80c2cf4450c6c3e4a8b9dbf240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+762178cae05504ea29edf63b8326a8f4817bf138")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
